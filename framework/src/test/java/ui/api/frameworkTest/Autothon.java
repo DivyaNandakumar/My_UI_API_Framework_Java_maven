9f1fbf43c13e2526ad5c9d1cb1cdabf51fe6c64c
shimilbm <shimilbm@motorola.com>
2019-08-21 12:46:34 +0530

    Code for the screenshot

---
+++ framework/src/test/java/ui/api/frameworkTest/Autothon.java
@@ -0,0 +1,28 @@
+package ui.api.frameworkTest;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Autothon {
+
+ 
+  @Test
+  public static void captureScreenMethod() throws Exception{
+	  String exePath = "src\\assets\\chromedriver.exe";
+	  System.setProperty("webdriver.chrome.driver", exePath);
+  System.setProperty("webdriver.gecko.driver","src\\assets\\geckodriver.exe");
+  WebDriver driver = new ChromeDriver();
+  driver.manage().window().maximize();
+  driver.get("https://www.youtube.com");
+  File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+  FileUtils.copyFile(screenshotFile, new File("src/test/java/ui/api/frameworkTest/Screenshots/YouTubeCapture.png"));
+  driver.close();
+  driver.quit(); 
+  }
+
+}
